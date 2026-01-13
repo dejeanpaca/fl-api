c773cdaeae8d2f6a0cb22ebb7acbe19a56279ec6
Dejan Boras <de.jean.7777@protonmail.com>
2026-01-13 23:45:15 +0100

    Update snp models

--- broque/stocks-sync/internal/types/types.go
+++ broque/stocks-sync/internal/types/types.go
@@ -3,22 +3,22 @@ package types
 import "time"
 
 type SNPConstituent struct {
-	symbol               string
-	security             string
-	gicsSector           string
-	gicsSubIndustry      string
-	headquartersLocation string
-	dateAdded            string
-	cik                  int
-	founded              int
+	Symbol               string    `json:"Symbol"`
+	Security             string    `json:"Security"`
+	GICSSector           string    `json:"GICS Sector"`
+	GICSSubIndustry      string    `json:"GICS SubIndustry"`
+	HeadquartersLocation string    `json:"Headquarters Location"`
+	DateAdded            time.Time `json:"Date Added"`
+	CIK                  int       `json:"CIK"`
+	Founded              int       `json:"Founded"`
 }
 
 type TDEODStock struct {
-	symbol    string
-	exchange  string
-	micCode   string
-	currency  string
-	datetime  string
-	timestamp time.Time
-	close     float64
+	Symbol    string    `json:"symbol"`
+	Exchange  string    `json:"exchange"`
+	MicCode   string    `json:"micCode"`
+	Currency  string    `json:"currency"`
+	Datetime  string    `json:"datetime"`
+	Timestamp time.Time `json:"timestamp"`
+	Close     float64   `json:"close"`
 }
