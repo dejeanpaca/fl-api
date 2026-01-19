938dfd371a4ff42f64e49321a31efa34020445de
Dejan Boras <de.jean.7777@protonmail.com>
2026-01-19 14:14:18 +0100

    Custom parsing of SNPConstituent fields

--- broque/stocks-sync/internal/types/types.go
+++ broque/stocks-sync/internal/types/types.go
@@ -1,16 +1,21 @@
 package types
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+)
 
 type SNPConstituent struct {
-	Symbol               string    `json:"Symbol"`
-	Security             string    `json:"Security"`
-	GICSSector           string    `json:"GICS Sector"`
-	GICSSubIndustry      string    `json:"GICS SubIndustry"`
-	HeadquartersLocation string    `json:"Headquarters Location"`
-	DateAdded            time.Time `json:"Date Added"`
-	CIK                  int       `json:"CIK"`
-	Founded              int       `json:"Founded"`
+	Symbol               string             `json:"Symbol"`
+	Security             string             `json:"Security"`
+	GICSSector           string             `json:"GICS Sector"`
+	GICSSubIndustry      string             `json:"GICS SubIndustry"`
+	HeadquartersLocation string             `json:"Headquarters Location"`
+	DateAdded            SNPConstituentTime `json:"Date Added"`
+	CIK                  int                `json:"CIK"`
+	Founded              string             `json:"Founded"`
 }
 
 type TDEODStock struct {
@@ -21,4 +26,97 @@ type TDEODStock struct {
 	Datetime  string    `json:"datetime"`
 	Timestamp time.Time `json:"timestamp"`
 	Close     float64   `json:"close"`
+}
+
+type SNPConstituentTime struct {
+	time.Time
+}
+
+func (t *SNPConstituentTime) UnmarshalJSON(b []byte) (err error) {
+	date, err := time.Parse(`"2006-01-02"`, string(b))
+	if err != nil {
+		t.Time = time.Now()
+		return nil
+	}
+	t.Time = date
+	return
+}
+
+func (p *SNPConstituent) UnmarshalJSON(b []byte) error {
+	var objMap map[string]*json.RawMessage
+	err := json.Unmarshal(b, &objMap)
+	if err != nil {
+		return err
+	}
+
+	var symbol string
+	err = json.Unmarshal(*objMap["Symbol"], &symbol)
+	if err != nil {
+		return err
+	}
+	p.Symbol = symbol
+
+	var cik int
+	err = json.Unmarshal(*objMap["CIK"], &cik)
+	if err != nil {
+		return err
+	}
+	p.CIK = cik
+
+	var foundedInt int
+	err = json.Unmarshal(*objMap["Founded"], &foundedInt)
+	if err != nil {
+		// age  is string
+		var foundedString string
+		err = json.Unmarshal(*objMap["Founded"], &foundedString)
+		if err != nil {
+			return err
+		}
+		p.Founded = foundedString
+
+	} else {
+		p.Founded = strconv.Itoa(foundedInt)
+	}
+
+	p.DateAdded = SNPConstituentTime{
+		Time: time.Time{},
+	}
+
+	if objMap["Date Added"] != nil {
+		_ = json.Unmarshal(*objMap["Date Added"], &p.DateAdded)
+	}
+
+	var security string
+	err = json.Unmarshal(*objMap["Security"], &security)
+	if err != nil {
+		return err
+	}
+	p.Security = security
+
+	var gicsSector string
+	err = json.Unmarshal(*objMap["GICS Sector"], &gicsSector)
+	if err != nil {
+		return err
+	}
+	p.GICSSector = gicsSector
+
+	if objMap["GICS SubIndustry"] != nil {
+		var gicsSubIndustry string
+		err = json.Unmarshal(*objMap["GICS SubIndustry"], &gicsSubIndustry)
+		if err != nil {
+			return err
+		}
+		p.GICSSubIndustry = gicsSubIndustry
+	}
+
+	var headquartersLocation string
+	err = json.Unmarshal(*objMap["Headquarters Location"], &headquartersLocation)
+	if err != nil {
+		return err
+	}
+	p.HeadquartersLocation = headquartersLocation
+
+	fmt.Printf("%+v", *p)
+
+	return nil
 }
